793b3a5379423375cb8a0c2355990730acc6b3dd
Michael Mockus <mmockus@gmail.com>
2021-07-19 18:52:19 -0500

    update docusaurus json

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,13 +5,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 module.exports = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://mmockus.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'mmockus', // Usually your GitHub org/user name.
+  projectName: 'mmockus.github.io', // Usually your repo name.
   themeConfig: {
     navbar: {
       title: 'My Site',
